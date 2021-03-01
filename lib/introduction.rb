4055dd9d60c2ca65ed5e4974fa39fa417b95cd09
Tim Quimbo <tquimbo1@gmail.com>
2021-03-01 14:41:18 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,3 +1,6 @@
-def introduction(name, language)
- puts "Hi, my name is #{name} and I am learning to program in #{language}."
+# def introduction(name, language)
+#  puts "Hi, my name is #{name} and I am learning to program in #{language}."
+# end
+
+def introduction(name)
 end
